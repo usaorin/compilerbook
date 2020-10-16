c6fccec4dc30d611e7b9b9f5e503d961f83f4bf0
Saori USAMI <usaorin@gmail.com>
2020-10-16 20:11:40 +0900

    Add mul, div and ()
    Add suuport for multiplication, division and priority
    using AST and stack operations.

--- mycc.c
+++ mycc.c
@@ -12,13 +12,31 @@ typedef enum {
 	TK_EOF,      // End of token
 } token_type_t;
 
+// Type of  Abstract Syntax Tree Node
+typedef enum {
+	ND_ADD, // +
+	ND_SUB, // -
+	ND_MUL, // *
+	ND_DIV, // /
+	ND_NUM, // integer
+} node_type_t;
+
+typedef struct node node_t;
 typedef struct token token_t;
 
+// Abstract Syntax Tree Node
+struct node {
+	node_type_t type; // node type
+	node_t *lhs;      // left-hand side
+	node_t *rhs;      // right-hand side
+	int val;          // Use when type is ND_NUM
+};
+// Token
 struct token {
 	token_type_t type; // Type of token
 	token_t *next;     // Next input token
-	int val;         // Value when type is TK_NUM
-	char *str;       // Token string
+	int val;           // Value when type is TK_NUM
+	char *str;         // Token string
 };
 
 // Current target token
@@ -47,6 +65,32 @@ void error_at(char *loc, char *fmt, ...) {
 	fprintf(stderr, fmt, ap);
 	fprintf(stderr, "\n");
 	exit(1);
+}
+
+// Create new node
+node_t *new_node(node_type_t type, node_t *lhs, node_t *rhs) {
+	node_t *node = calloc(1, sizeof(node_t));
+	node->type = type;
+	node->lhs = lhs;
+	node->rhs = rhs;
+	return node;
+}
+
+// Create new number node
+node_t *new_node_num(int val) {
+	node_t *node = calloc(1, sizeof(node_t));
+	node->type = ND_NUM;
+	node->val = val;
+	return node;
+}
+
+// Create new token.
+token_t *new_token(token_type_t type, token_t *cur, char *str) {
+	token_t *tok = calloc(1, sizeof(token_t));
+	tok->type = type;
+	tok->str = str;
+	cur->next = tok;
+	return tok;
 }
 
 // Check expect operator symbol and consume a token.
@@ -80,13 +124,77 @@ bool at_eof() {
 	return cur_token->type == TK_EOF;
 }
 
-// Create new token.
-token_t *new_token(token_type_t type, token_t *cur, char *str) {
-	token_t *tok = calloc(1, sizeof(token_t));
-	tok->type = type;
-	tok->str = str;
-	cur->next = tok;
-	return tok;
+node_t *expr();
+
+// primary = "(" expr ")" | num
+node_t *primary() {
+	// "(" expr ")"
+	if (consume('(')) {
+		node_t *node = expr();
+		expect_op(')');
+		return node;
+	}
+	// num
+	return new_node_num(expect_number());
+}
+// mul = primary ("*" primary | "/" primary)*
+node_t *mul() {
+	node_t *node = primary();
+
+	for (;;) {
+		if (consume('*'))
+			node = new_node(ND_MUL, node, primary());
+		else if (consume('/'))
+			node = new_node(ND_DIV, node, primary());
+		else
+			return node;
+	}
+}
+
+// expr = mul ("+" mul | "-" mul)*
+node_t *expr() {
+	node_t *node = mul();
+
+	for (;;) {
+		if (consume('+'))
+			node = new_node(ND_ADD, node, mul());
+		else if (consume('-'))
+			node = new_node(ND_SUB, node, mul());
+		else
+			return node;
+	}
+}
+
+// Stack operation
+void gen(node_t *node) {
+	if (node->type == ND_NUM) {
+		printf("  push %d\n", node->val);
+		return;
+	}
+
+	gen(node->lhs);
+	gen(node->rhs);
+
+	printf("  pop rdi\n");
+	printf("  pop rax\n");
+
+	switch (node->type) {
+	case ND_ADD:
+		printf("  add rax, rdi\n");
+		break;
+	case ND_SUB:
+		printf("  sub rax, rdi\n");
+		break;
+	case ND_MUL:
+		printf("  imul rax, rdi\n");
+		break;
+	case ND_DIV:
+		printf("  cqo\n");
+		printf("  idiv rdi\n");
+		break;
+	}
+
+	printf("  push rax\n");
 }
 
 // Tokenize the target string
@@ -103,7 +211,7 @@ token_t *tokenize() {
 			continue;
 		}
 
-		if (*p == '+' || *p == '-') {
+		if (strchr("+-*/()", *p)) {
 			cur = new_token(TK_RESERVED, cur, p++);
 			continue;
 		}
@@ -127,31 +235,22 @@ main(int argc, char **argv) {
 	if (argc != 2)
 		error("The number of arguments is incorrect.\n");
 
+	// Tokenize and parse.
 	user_input = argv[1];
-	cur_token = tokenize();
+	cur_token = tokenize(user_input);
+	node_t *node = expr();
 
 	// Outputs the first part of the assembly.
 	printf(".intel_syntax noprefix\n");
 	printf(".globl main\n");
 	printf("main:\n");
 
-	// Must start with a number
-	int n = expect_number();
-	if (n < 0) {
-		error("Must start with a number.");
-	}
-	printf("  mov rax, %d\n", n);
+	// Generate code from AST
+	gen(node);
 
-	while (!at_eof()) {
-		if (consume('+')) {
-			printf("  add rax, %d\n", expect_number());
-			continue;
-		}
-
-		expect_op('-');
-		printf("  sub rax, %d\n", expect_number());
-	}
-
+	// Pop from the top of the stack and put it in rax.
+	printf("  pop rax\n");
 	printf("  ret\n");
+
 	return 0;
 }
